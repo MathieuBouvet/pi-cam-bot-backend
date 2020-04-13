aa2fa315f6d684785125fff9c8a56525db110e2f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-13 23:01:11 +0200

    Implement finding existing process

--- utils/processFinder.js
+++ utils/processFinder.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  return null;
+const { exec } = require("child_process");
+module.exports = (name) => {
+  return new Promise((resolve, reject) => {
+    exec(`ps ax | grep -w ${name} | grep -v grep`, (err, stdout) => {
+      if (err) {
+        return reject(err);
+      }
+      const firstResult = stdout.split("\n").filter((line) => line !== "")[0];
+      const pid = firstResult.trim().split(" ")[0];
+      resolve(parseInt(pid));
+    });
+  });
 };
