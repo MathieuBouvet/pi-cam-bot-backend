b41a02e9de9e7e86ee8592a61d55117086c2524d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-13 23:04:57 +0200

    Implement return null on no result
    checking for error code 1 because grep return 1 when no matches are
    found

--- utils/processFinder.js
+++ utils/processFinder.js
@@ -3,7 +3,7 @@ module.exports = (name) => {
   return new Promise((resolve, reject) => {
     exec(`ps ax | grep -w ${name} | grep -v grep`, (err, stdout) => {
       if (err) {
-        return reject(err);
+        return err.code === 1 ? resolve(null) : reject(err);
       }
       const firstResult = stdout.split("\n").filter((line) => line !== "")[0];
       const pid = firstResult.trim().split(" ")[0];
