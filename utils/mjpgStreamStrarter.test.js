b54edb49048d0ad3a05ee143032e809b429dd4b3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-13 23:54:32 +0200

    Test the mjpg stream starter

---
+++ utils/mjpgStreamStrarter.test.js
@@ -0,0 +1,29 @@
+const startMjpgStream = require("./mjpgStreamStarter");
+const findProcess = require("./processFinder");
+const childProcess = require("child_process");
+const { ChildProcess, exec } = childProcess;
+
+describe("Mjpg stream starter", () => {
+  afterEach(async () => {
+    const process = await findProcess("mjpg_streamer");
+    return new Promise((resolve, reject) => {
+      if (!process) {
+        return resolve();
+      }
+      exec("kill " + process, (err) => {
+        return err ? reject(err) : resolve();
+      });
+    });
+  });
+  it("should resolve to a child process once the streamer is started", async () => {
+    const streamerProcess = await startMjpgStream();
+    expect(streamerProcess).toBe(expect.any(ChildProcess));
+    expect(await findProcess("mjpg_streamer")).toBe(streamerProcess.pid);
+  });
+  it("should reject with an error when unable to start", async () => {
+    const process = startMjpgStream(
+      "mjpg_streamer -i bad_input.so -o output_http.so"
+    );
+    await expect(process).rejects.toThrow();
+  });
+});
