3ece4b449bf7494dce1e74e37d9c8d4e1417ca90
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-14 16:12:02 +0200

    Implement error in command rejection

--- utils/mjpgStreamStarter.js
+++ utils/mjpgStreamStarter.js
@@ -1,3 +1,22 @@
-module.exports = () => {
-  return null;
+const { spawn } = require("child_process");
+
+module.exports = async (commandString = process.env.MJPG) => {
+  const [command, ...args] = commandString.split(" ");
+  const process = spawn(command, args);
+  return commandError(process).promise;
 };
+
+function commandError(process) {
+  let exitHandler = null;
+  const commandErrorPromise = new Promise((resolve, reject) => {
+    exitHandler = () => reject(new Error("Unable to start the process"));
+    process.on("exit", exitHandler);
+  });
+  const clean = () => {
+    process.removeListener("exit", exitHandler);
+  };
+  return {
+    promise: commandErrorPromise,
+    clean,
+  };
+}
