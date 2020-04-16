80b06e435ae291c6dbac3e8f1b0cb0a0126710e7
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-17 00:29:37 +0200

    Implement camera service

--- services/camera.service.js
+++ services/camera.service.js
@@ -1,5 +1,12 @@
-async function update() {
-  return null;
+const streamer = require("../utils/streamerProcess");
+const withSynchronization = require("../utils/synchronizer")();
+
+const synchronizedStart = withSynchronization(streamer.start);
+const synchronizedStop = withSynchronization(streamer.stop);
+
+async function update(status) {
+  const action = status.started ? synchronizedStart : synchronizedStop;
+  return action();
 }
 
 module.exports = {
