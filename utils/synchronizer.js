04f43291b29ad623b6835a935f65743bfa62e897
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-16 19:22:37 +0200

    Implement the synchronizer

--- utils/synchronizer.js
+++ utils/synchronizer.js
@@ -1,5 +1,37 @@
 function synchronizer() {
-  return null;
+  let idle = true;
+  const awaiting = [];
+
+  const beeingIdle = async () => {
+    if (idle) {
+      idle = false;
+      return Promise.resolve();
+    }
+    return new Promise((resolve) => {
+      awaiting.push(resolve);
+    });
+  };
+
+  const taskFinished = () => {
+    if (awaiting.length === 0) {
+      idle = true;
+    } else {
+      const resolveNextAwaiting = awaiting.shift();
+      resolveNextAwaiting();
+    }
+  };
+
+  return function withSynchronization(action) {
+    return async () => {
+      await beeingIdle();
+      try {
+        const result = await action();
+        return result;
+      } finally {
+        taskFinished();
+      }
+    };
+  };
 }
 
 module.exports = synchronizer;
