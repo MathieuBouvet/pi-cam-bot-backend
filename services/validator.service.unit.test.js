e5e6e413b5fe089ce4b7923c8ba26b99ee8d6e96
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-18 18:50:38 +0200

    Add test for camera validator

---
+++ services/validator.service.unit.test.js
@@ -0,0 +1,33 @@
+const validator = require("./validator.service");
+const { Http400 } = require("../utils/errors");
+
+describe("camera validator", () => {
+  const cameraValidator = validator.camera;
+  it.each([[{ started: true }], [{ started: false }]])(
+    "should return the input when passing validation",
+    (value) => {
+      expect(cameraValidator(value)).toEqual(value);
+    }
+  );
+  it.each([
+    [null],
+    [42],
+    ["42"],
+    [() => null],
+    [[]],
+    [true],
+    [{ invalid: true }],
+    [{ started: 42 }],
+    [{ started: "42" }],
+    [{ started: null }],
+    [{ started: undefined }],
+    [{ started: () => null }],
+    [{ started: [] }],
+    [{ started: true, invalid: false }],
+  ])(
+    "should throw an Http400 error when input is not {started: Boolean}",
+    (input) => {
+      expect(() => cameraValidator(input)).toThrow(Http400);
+    }
+  );
+});
