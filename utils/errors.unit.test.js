b13c202999de71b60cd460ea2752aab273495458
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-18 18:50:07 +0200

    Test Http400 custom error

---
+++ utils/errors.unit.test.js
@@ -0,0 +1,14 @@
+const { Http400 } = require("./errors");
+
+describe("Http400 error", () => {
+  const http400Instance = new Http400("test http 400");
+  it("should be an instance of Error", () => {
+    expect(http400Instance).toBeInstanceOf(Error);
+  });
+  it("should be named 400 - Bad Request", () => {
+    expect(http400Instance.name).toBe("400 - Bad Request");
+  });
+  it("should set its message correctly", () => {
+    expect(http400Instance.message).toBe("test http 400");
+  });
+});
