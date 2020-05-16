ac1f06fe4dcb0dbea542d8bb7ea3dbebd0dcc6ce
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-16 17:17:01 +0100

    Implement forward action

--- utils/wheels.ts
+++ utils/wheels.ts
@@ -11,6 +11,12 @@ export interface Directions {
   left: boolean;
   right: boolean;
 }
+interface actionOnWheel {
+  (wheel: wheel): Gpio[];
+}
+type WheelsAction = {
+  [key in keyof RobotWheels]: actionOnWheel;
+};
 
 function activate(gpio: Gpio): void {
   gpio.writeSync(1);
@@ -20,9 +26,20 @@ function deactivate(gpio: Gpio): void {
   gpio.writeSync(0);
 }
 
+const nothing: actionOnWheel = (wheel) => [];
+const forward: actionOnWheel = (wheel) => [wheel[0]];
+
+function getWheelsAction({ up, down, left, right }: Directions): WheelsAction {
+  if (up) {
+    return { left: forward, right: forward };
+  }
+  return { left: nothing, right: nothing };
+}
+
 export function gpiosToActivate(
   directions: Directions,
   wheels: RobotWheels
 ): Gpio[] {
-  return [];
+  const actionsOn = getWheelsAction(directions);
+  return [...actionsOn.left(wheels.left), ...actionsOn.right(wheels.right)];
 }
