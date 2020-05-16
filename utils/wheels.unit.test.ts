5f7a94bccb669b0af29bed1aa4152b5cafbfe410
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-16 19:06:06 +0100

    Use createMovement to simplify tests

--- utils/wheels.unit.test.ts
+++ utils/wheels.unit.test.ts
@@ -1,5 +1,10 @@
 import { Gpio } from "onoff";
-import { gpiosToActivate, RobotWheels, Movement } from "./wheels";
+import {
+  gpiosToActivate,
+  RobotWheels,
+  Movement,
+  createMovement,
+} from "./wheels";
 
 const testGpios = [
   new Gpio(11, "out"),
@@ -13,177 +18,34 @@ const testRobotWheels: RobotWheels = {
   right: [testGpios[2], testGpios[3]],
 };
 
-it.each([
-  [
-    "move forward",
-    {
-      up: true,
-      down: false,
-      left: false,
-      right: false,
-    },
-    [testGpios[0], testGpios[2]],
-  ],
-  [
-    "move backward",
-    {
-      up: false,
-      down: true,
-      left: false,
-      right: false,
-    },
-    [testGpios[1], testGpios[3]],
-  ],
-  [
-    "move left",
-    {
-      up: true,
-      down: false,
-      left: true,
-      right: false,
-    },
-    [testGpios[2]],
-  ],
-  [
-    "move right",
-    {
-      up: true,
-      down: false,
-      left: false,
-      right: true,
-    },
-    [testGpios[0]],
-  ],
-  [
-    "rotate left",
-    {
-      up: false,
-      down: false,
-      left: true,
-      right: false,
-    },
-    [testGpios[1], testGpios[2]],
-  ],
-  [
-    "rotate right",
-    {
-      up: false,
-      down: false,
-      left: false,
-      right: true,
-    },
-    [testGpios[0], testGpios[3]],
-  ],
+it.each<[string, (keyof Movement)[], Gpio[]]>([
+  ["move forward", ["up"], [testGpios[0], testGpios[2]]],
+  ["move backward", ["down"], [testGpios[1], testGpios[3]]],
+  ["move left", ["up", "left"], [testGpios[2]]],
+  ["move right", ["up", "right"], [testGpios[0]]],
+  ["rotate left", ["left"], [testGpios[1], testGpios[2]]],
+  ["rotate right", ["right"], [testGpios[0], testGpios[3]]],
 ])(
   "should return the list of gpio to activate to %s",
-  (description: string, movement: Movement, expected: Gpio[]) => {
+  (description, directions, expected: Gpio[]) => {
+    const movement = createMovement(...directions);
     expect(gpiosToActivate(movement, testRobotWheels)).toEqual(expected);
   }
 );
 
-it.each([
-  [
-    "UP and DOWN",
-    {
-      up: true,
-      down: true,
-      left: false,
-      right: false,
-    },
-  ],
-  [
-    "LEFT and RIGHT",
-    {
-      up: false,
-      down: false,
-      left: true,
-      right: true,
-    },
-  ],
-  [
-    "DOWN and LEFT",
-    {
-      up: false,
-      down: true,
-      left: true,
-      right: false,
-    },
-  ],
-  [
-    "DOWN and RIGHT",
-    {
-      up: false,
-      down: true,
-      left: false,
-      right: true,
-    },
-  ],
-  [
-    "UP and DOWN and LEFT",
-    {
-      up: true,
-      down: true,
-      left: true,
-      right: false,
-    },
-  ],
-  [
-    "DOWN and LEFT and RIGHT",
-    {
-      up: false,
-      down: true,
-      left: true,
-      right: true,
-    },
-  ],
-  [
-    "UP and LEFT and RIGHT",
-    {
-      up: true,
-      down: false,
-      left: true,
-      right: true,
-    },
-  ],
-  [
-    "UP and DOWN and RIGHT",
-    {
-      up: true,
-      down: true,
-      left: false,
-      right: true,
-    },
-  ],
-  [
-    "UP and DOWN and LEFT",
-    {
-      up: true,
-      down: true,
-      left: true,
-      right: false,
-    },
-  ],
-  [
-    "all at once",
-    {
-      up: true,
-      down: true,
-      left: true,
-      right: true,
-    },
-  ],
-  [
-    "none",
-    {
-      up: false,
-      down: false,
-      left: false,
-      right: false,
-    },
-  ],
-])(
-  "should return empty list for %s",
-  (description: string, movement: Movement) => {
-    expect(gpiosToActivate(movement, testRobotWheels)).toEqual([]);
-  }
-);
+it.each<[string, (keyof Movement)[]]>([
+  ["UP and DOWN", ["up", "down"]],
+  ["LEFT and RIGHT", ["left", "right"]],
+  ["DOWN and LEFT", ["down", "left"]],
+  ["DOWN and RIGHT", ["down", "right"]],
+  ["UP and DOWN and LEFT", ["up", "down", "left"]],
+  ["DOWN and LEFT and RIGHT", ["down", "left", "right"]],
+  ["UP and LEFT and RIGHT", ["up", "left", "right"]],
+  ["UP and DOWN and RIGHT", ["up", "down", "right"]],
+  ["UP and DOWN and LEFT", ["up", "down", "left"]],
+  ["all at once", ["up", "down", "left", "right"]],
+  ["none", []],
+])("should return empty list for %s", (description, directions) => {
+  const movement = createMovement(...directions);
+  expect(gpiosToActivate(movement, testRobotWheels)).toEqual([]);
+});
