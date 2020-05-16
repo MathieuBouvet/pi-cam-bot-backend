759312ac6349ef4e10fc26d15318237210b52849
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-16 20:05:22 +0100

    Implement movement validator

--- services/validator.service.ts
+++ services/validator.service.ts
@@ -1,5 +1,6 @@
 import { Http400 } from "../utils/errors";
 import { CameraStatus } from "../utils/streamerProcess";
+import { Movement } from "../utils/wheels";
 
 function camera(input: any): CameraStatus {
   if (
@@ -12,6 +13,26 @@ function camera(input: any): CameraStatus {
   }
   throw new Http400("invalid camera input");
 }
+
+function isMovement(input: any): input is Movement {
+  if (input == null) {
+    return false;
+  }
+  const casted = input as Movement;
+  return (
+    typeof casted.up === "boolean" &&
+    typeof casted.down === "boolean" &&
+    typeof casted.left === "boolean" &&
+    typeof casted.right === "boolean"
+  );
+}
+function movement(input: any): Movement {
+  if (!isMovement(input) || Object.keys(input).length !== 4) {
+    throw new Http400("invalid movement input");
+  }
+  return input;
+}
 export default {
   camera,
+  movement,
 };
